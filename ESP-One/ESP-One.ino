f624414ba1dd82fb94dbd74c943cff4c9306e0c0
Joseph Desktop <joseph@schafers.com>
2020-11-17 22:32:51 -0700

    work in progress

--- ESP-One/ESP-One.ino
+++ ESP-One/ESP-One.ino
@@ -1,34 +1,80 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
 #define TEMP1_READ_PIN 2
 #define MS_TO_SEC 1000
 #define ONE_SECOND (1 * MS_TO_SEC)
 #define FIVE_SECONDS (5 * MS_TO_SEC)
 #define HEAT_ENABLE_AT 21
 #define HEAT_DISABLE_AT 24
+#define ESP_TWO_CHANNEL 0
+#define PEER_ADDR_MEM_BYTES 6
+#define TRANSMIT_LED_PIN 12
+#define RESET 0
 
-static int tempVoltRead = 0;
-static int tempInCelsius = 0;
+const uint8_t ESP_Two_address[] = {0x24, 0x6F, 0x28, 0xB2, 0xD6, 0x84};
+
+static uint32_t tempVoltRead = RESET;
+static uint32_t tempInCelsius = RESET;
+static uint32_t swapper = RESET;
 static bool heatOnFlag = false;
+
 enum ESP_One_st {temp_reading_st, transmit_st} ESP_One_current_st;
 
-static int volt_to_celsius(int x) {
+static uint32_t volt_to_celsius(uint32_t x) {
   return ((x - 500) / 10);
+}
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(TEMP1_READ_PIN, INPUT);
+  pinMode(TRANSMIT_LED_PIN, OUTPUT);
   delay(ONE_SECOND);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, ESP_Two_address, PEER_ADDR_MEM_BYTES);
+  peerInfo.channel = ESP_TWO_CHANNEL;  
+  peerInfo.encrypt = true;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
  
 void loop() {
   // put your main code here, to run repeatedly:
   tempVoltRead = analogRead(TEMP1_READ_PIN);
-  Serial.print("Pin 2 Read: ");
-  Serial.println(tempVoltRead);
   tempInCelsius = volt_to_celsius(tempVoltRead);
-  Serial.println(tempInCelsius);
-  
+  if (swapper >= 3) {
+    swapper = RESET;
+    heatOnFlag = !heatOnFlag;
+    esp_err_t result = esp_now_send(ESP_Two_address, (uint8_t *) &heatOnFlag, sizeof(heatOnFlag));
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+    }
+    Serial.print("Transmitting LOOP ");
+    Serial.println(heatOnFlag);
+  }
+  swapper++;
   
   switch (ESP_One_current_st) {
   case temp_reading_st:
@@ -41,6 +87,7 @@ void loop() {
     break;
    case transmit_st:
     Serial.println("Transmitting");
+    esp_now_send(ESP_Two_address, (uint8_t *) heatOnFlag, sizeof(heatOnFlag));
     ESP_One_current_st = temp_reading_st;
     break;
   }
